2ea045f218094725ef00c4fa81835f4dac83146b
the-moliver <michaeloliver@berkeley.edu>
2014-09-04 02:20:57 -0700

    fix quartic loss def

--- src/losses.jl
+++ src/losses.jl
@@ -6,7 +6,7 @@ squared_loss(y, t) = 0.5 * norm(y .- t).^2 # L(y, t) = .5 || y - t ||^2 = .5 (y 
 squared_lossd(y, t) = y .- t # d/dx L(y, t) = (y - t)
 
 
-quartic_loss(y, t) = 0.25 * norm(y .- t).^4 
+quartic_loss(y, t) = 0.25 * sum(abs(y .- t).^4) 
 quartic_lossd(y, t) = (y .- t).^3 
 
 function poisson_loss(y, t)
