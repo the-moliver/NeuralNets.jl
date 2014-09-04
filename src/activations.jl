02dd58232835dba622e22cde2f86194230dcd4a2
the-moliver <michaeloliver@berkeley.edu>
2014-09-04 14:36:56 -0700

    Fix so correct dimension zeroed

--- src/activations.jl
+++ src/activations.jl
@@ -43,10 +43,8 @@ end
 
 function donrelud(x,idx) 
 	a = (x .> 0) + 0.
-	print(size(a))
-	print(size(idx))
-	a[:,idx[1:(.5*length(idx))]] = 0.
-	a[:,idx[(.5*length(idx)+1):end]] .*= 2.0
+	a[idx[1:(.5*length(idx))],:] = 0.
+	a[idx[(.5*length(idx)+1):end],:] .*= 2.0
 	a
 end
 
