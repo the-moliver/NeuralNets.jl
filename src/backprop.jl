1e81b445823bfea9e0719ebcd9094f9d618e680e
Michael Oliver <michael.d.oliver@gmail.com>
2014-09-13 19:22:14 -0700

    Attempt to speed up errprop

--- src/backprop.jl
+++ src/backprop.jl
@@ -213,7 +213,11 @@ function errprop!(w::Array{Float32,3}, d::Array{Float32,3}, deltas)
 	deltas.d[:] = 0.
 	for ti=1:size(w,3)
 	    for ti2 = 1:size(d,3)
-	    	deltas.d[:,:,ti+ti2-1] += w[:,:,ti]'*d[:,:,ti2];
+	    	for ww = 1:size(w,2)
+	    		for dd = 1:size(d,2)
+	    			deltas.d[ww,dd,ti+ti2-1] += w[:,ww,ti]'*d[:,dd,ti2];
+	    		end
+	    	end
 	    end
 	end
 	deltas.d
