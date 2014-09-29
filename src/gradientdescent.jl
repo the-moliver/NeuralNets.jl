8cd63aa60754fd377c8500fa04f29274ac6dab93
Michael Oliver <michael.d.oliver@gmail.com>
2014-09-29 16:13:37 -0700

    fix second colon

--- src/gradientdescent.jl
+++ src/gradientdescent.jl
@@ -80,7 +80,7 @@ end
 function mini_batch!(x,t,x_batch,t_batch,fitpoints, tdmlp::TDMLP)
   delays = tdmlp.delays
   t_batch[:,:] = t[:,fitpoints]
-  t_batch[,fitpoints.<=tdmlp.delays] = NaN
+  t_batch[:,fitpoints.<=tdmlp.delays] = NaN
   for i=0:delays
     #x_batch[:,:,i+1] = x[:,max(fitpoints,1)]
     for ii = [1:length(fitpoints)]
