5a06ca8fc8150b2a16428e3c05a349009b014d63
Michael Oliver <michael.d.oliver@gmail.com>
2015-02-09 10:01:12 -0800

    update mnist example

--- examples/MNIST_example.jl
+++ examples/MNIST_example.jl
@@ -18,25 +18,23 @@ trainstim = trainstim ./ sX
 ind = size(trainstim,1)
 outd = size(trainval,1)
 
-layer_sizes = [ind, 200, 200, outd]
-act   = [donrelu,nrelu, softmaxact];
+
+layer_sizes = [ind, 500, 50, 200, outd]
+act   = [nrelu,nrelu,nrelu, softmaxact];
 
 trainstim = convert(Array{Float32}, trainstim)
 trainval = convert(Array{Float32}, trainval)
 
 
 mlp = MLP(layer_sizes, act);
-mlp = rmsproptrain(mlp, trainstim, trainval, learning_rate=.001, learning_rate_factor=.995, maxiter=1, batch_size = 100, loss=xent_loss, maxnorm=1.)
+mlp = rmsproptrain(mlp, trainstim, trainval, learning_rate=.001, learning_rate_factor=.96, maxiter=200, batch_size = 100, loss=xent_loss, maxnorm=1.)
 
-mlp = rmsproptrain(mlp, trainstim, trainval, learning_rate=.001, learning_rate_factor=.995, maxiter=100, batch_size = 100, loss=xent_loss,maxnorm=1.)
+# for ii=1:3
+# mlp.net[ii].a = relu
+# mlp.net[ii].ad = relud
+# end
 
-
-for ii=1:3
-mlp.net[ii].a = relu
-mlp.net[ii].ad = relud
-end
-
-mlp = rmsproptrain(mlp, trainstim, trainval, learning_rate=.001, learning_rate_factor=.98, maxiter=100, batch_size = 100, loss=xent_loss)
+# mlp = rmsproptrain(mlp, trainstim, trainval, learning_rate=.001, learning_rate_factor=.9, maxiter=100, batch_size = 100, loss=xent_loss, maxnorm=1.)
 
 
 O = prop(mlp, trainstim);
